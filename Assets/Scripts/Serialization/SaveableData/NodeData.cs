1b02437b74a24d15ab41632f74fed4f7e470863c
Aidan Fu <Aidan.fu000@gmail.com>
2021-03-13 13:02:37 -0500

    Organized files in Serialization folder

---
+++ Assets/Scripts/Serialization/SaveableData/NodeData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FarmSim.Serialization
+{
+
+    [System.Serializable]
+    public class NodeData
+    {
+        public bool IsOccupied = false;
+        public Vector2 Position;
+
+        public readonly int x = 0;
+        public readonly int y = 0;
+    }
+}
